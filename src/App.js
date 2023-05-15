a1ce8519a0bfd9a045262bbbe9a4be4e26bde820
katerina-shche <asti.katerina@gmail.com>
2023-05-15 16:26:51 +0200

    buttons

--- src/App.js
+++ src/App.js
@@ -3,7 +3,32 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div id='calculator'>
+        <div id='display'> display
+          <div id='input'></div>
+          <div id='output'></div>
+        </div>
+        <div id='buttons'>
+          <button id='clear'>AC</button>
+          <button id='divide'>/</button>
+          <button id='multiply'>x</button>
+          <button id='seven'>7</button>
+          <button id='eight'>8</button>
+          <button id='nine'>9</button>
+          <button id='substruct'>-</button>
+          <button id='four'>4</button>
+          <button id='five'>5</button>
+          <button id='six'>6</button>
+          <button id='add'>+</button>
+          <button id='one'>1</button>
+          <button id='two'>2</button>
+          <button id='three'>3</button>
+          <button id='zero'>0</button>
+          <button id='decimal'>.</button>
+          <button id='equals'>=</button>
+        </div>
+      </div>
+      <div id='author'>by katerina-shche</div>
     </div>
   );
 }
