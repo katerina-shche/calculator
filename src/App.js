f1c4db5b7dd766467664d072110a11d73ed7de78
katerina-shche <asti.katerina@gmail.com>
2023-05-18 16:09:01 +0200

    handleOperator finished

--- src/App.js
+++ src/App.js
@@ -25,7 +25,10 @@ function App() {
   const handleOperator = (e) => {
     if (e.target.value !== '-' && input === '') {
       console.log('this operator is not alowed now')
-    }  else if ((/[+\-*/.]/g).test(input.slice(-1))) {
+    } else if ((/[+/-*/]/g).test(input.slice(-1)) && e.target.value === '-') {
+      setInput(input+e.target.value)
+      setOutput(e.target.value)
+    } else if ((/[+\-*/.]/g).test(input.slice(-1))) {
       setInput(input.slice(0, -1)+e.target.value)
       setOutput(e.target.value)
     } else {
